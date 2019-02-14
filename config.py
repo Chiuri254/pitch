362f3d5356f36006e71c7a844c93de9632c16518
Chiuri254 <joseshiory@gmail.com>
2019-02-14 10:40:36 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class config:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://joseph:pdatabase@localhost/pitch'
-    SECRET_KEY = "steve"
+    SECRET_KEY = "32451618"
     # MAIL_SERVER = 'smtp.gmail.com'
     # MAIL_PORT = 587
     # MAIL_USE_TLS = True
